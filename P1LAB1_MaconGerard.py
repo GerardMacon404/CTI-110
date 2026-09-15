ae6d48e9eb01ded500990e9d91801e5ede4bb5df
GerardMacon404 <macong0248@student.faytechcc.edu>
2026-09-15 17:17:20 -0400

    first FTCC python assignment!! :D
    taking input from user and printing output

---
+++ P1LAB1_MaconGerard.py
@@ -0,0 +1,9 @@
+# Gerard Macon
+# 09/15/2026
+# P1LAB1
+# This program asks the user for their first and last name and displays a welcome message.
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+print("Welcome," ,first_name, last_name + "!" , "Welcome to CTI-110!")
